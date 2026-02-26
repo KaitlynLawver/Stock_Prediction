db1186c54689610ecf2a4ca3439fb0d3965b4b26
KaitlynLawver <k.e.lawver@tcu.edu>
2026-02-26 13:31:28 -0600

    Update feature_utils.py

--- src/feature_utils.py
+++ src/feature_utils.py
@@ -20,33 +20,37 @@ def extract_features():
     
     START_DATE = (datetime.date.today() - datetime.timedelta(days=365)).strftime("%Y-%m-%d")
     END_DATE = datetime.date.today().strftime("%Y-%m-%d")
-    stk_tickers = ['NFLX', 'DIS', 'META']
-    ccy_tickers = ['DEXJPUS', 'DEXUSUK']
-    idx_tickers = ['^GSPC', '^IXIC', '^VIX']
+    stk_tickers = ['MPWR', 'APPL'] #['MSFT', 'IBM', 'GOOGL']
+    #ccy_tickers = ['DEXJPUS', 'DEXUSUK']
+    #idx_tickers = ['^GSPC', '^IXIC', '^VIX']
     
     stk_data = yf.download(stk_tickers, start=START_DATE, end=END_DATE, auto_adjust=False)
     #stk_data = web.DataReader(stk_tickers, 'yahoo')
-    ccy_data = web.DataReader(ccy_tickers, 'fred', start=START_DATE, end=END_DATE)
-    idx_data = yf.download(idx_tickers, start=START_DATE, end=END_DATE, auto_adjust=False)
+    #ccy_data = web.DataReader(ccy_tickers, 'fred', start=START_DATE, end=END_DATE)
+    #idx_data = yf.download(idx_tickers, start=START_DATE, end=END_DATE, auto_adjust=False)
 
-    Y = np.log(stk_data.loc[:, ('Adj Close', 'NFLX')]).diff(return_period).shift(-return_period)
-    Y.name = 'NFLX_future_return'
+    #Y = np.log(stk_data.loc[:, ('Adj Close', 'NFLX')]).diff(return_period).shift(-return_period)
+    Y = stk_data.loc[:, ('Adj Close', 'APPL')])
+    Y.name = 'APPL'
+
+    X = stk_data.loc[:, ('Adj Close', 'MPWR')])
+    X.name = 'MPWR'
     
-    X1 = np.log(stk_data.loc[:, ('Adj Close', ('DIS', 'META'))]).diff(return_period)
-    X1.columns = X1.columns.droplevel()
-    X2 = np.log(ccy_data).diff(return_period)
+    #X1 = np.log(stk_data.loc[:, ('Adj Close', ('DIS', 'META'))]).diff(return_period)
+    #X1.columns = X1.columns.droplevel()
+    #X2 = np.log(ccy_data).diff(return_period)
     # Use only Adjusted Close prices from index data
     idx_adj_close = idx_data['Adj Close']
     
     # Compute log returns
-    X3 = np.log(idx_adj_close).diff(return_period)
+    #X3 = np.log(idx_adj_close).diff(return_period)
     
-    X = pd.concat([X1, X2, X3], axis=1)
+    #X = pd.concat([X1, X2, X3], axis=1)
     
     dataset = pd.concat([Y, X], axis=1)
     
     # Align everything to stock trading dates
-    dataset = dataset.loc[stk_data.index]
+    #dataset = dataset.loc[stk_data.index]
     
     # Forward fill macro / FX data
     dataset = dataset.ffill()
@@ -59,7 +63,7 @@ def extract_features():
     #dataset.to_csv(r"./test_data.csv")
     features = dataset.sort_index()
     features = features.reset_index(drop=True)
-    features = features.iloc[:,1:]
+    #features = features.iloc[:,1:]
     return features
 
 
@@ -79,6 +83,7 @@ def get_bitcoin_historical_prices(days = 60):
     df['Date'] = pd.to_datetime(df['Timestamp'], unit='ms').dt.normalize()
     df = df[['Date', 'Close Price (USD)']].set_index('Date')
     return df
+
 
 
 
