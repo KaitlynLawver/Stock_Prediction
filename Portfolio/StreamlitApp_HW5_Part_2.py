028a370fb9e8dced97947cd6f84e1350e4c37be3
KaitlynLawver <k.e.lawver@tcu.edu>
2026-04-09 14:35:07 -0500

    Update StreamlitApp_HW5_Part_2.py

--- Portfolio/StreamlitApp_HW5_Part_2.py
+++ Portfolio/StreamlitApp_HW5_Part_2.py
@@ -95,7 +95,7 @@ def call_model_api(input_df):
         endpoint_name=MODEL_INFO["endpoint"],
         sagemaker_session=sm_session,
         serializer=JSONSerializer(), 
-        deserializer=NumpyDeserializer() 
+        deserializer=JSONDeserializer() 
     )
 
     try:
